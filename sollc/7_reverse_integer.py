fe61f4299a41ef908a6ff280973d9d68f21de50a
zdane <155510199+zidanej@users.noreply.github.com>
2024-06-26 15:42:17 +0530

    reverse integer

---
+++ sollc/7_reverse_integer.py
@@ -0,0 +1,25 @@
+def reverse(x:int):
+    a = str(x)
+    if(a[0] =="-"):
+        b = a[1:]
+        c = '-'+b[::-1]
+        c = int(c)
+        if(c>2**31):
+            return 0
+        if(c<-2**31):
+            return 0
+        else:
+            return c
+    else:
+        b = a[::-1]
+        b = int(b)
+        if(b>2**31):
+            return 0
+        if(b<-2**31):
+            return 0
+        else:
+            return b
+
+
+n = int(input("Enter the number to reverse: "))
+print(reverse(n))
